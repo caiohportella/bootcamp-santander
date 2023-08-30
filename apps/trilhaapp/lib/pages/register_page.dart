4cdcc1fe15c4c1ce3c8e0ba0c52192804a33adcf
Caio Portella <caiohportella@gmail.com>
2023-08-30 00:21:07 -0300

    feat: added slider, dropdown and select menus to form

--- apps/trilhaapp/lib/pages/register_page.dart
+++ apps/trilhaapp/lib/pages/register_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:trilhaapp/service/repositories/experience_years_repository.dart';
+import 'package:trilhaapp/service/repositories/level_repository.dart';
+import 'package:trilhaapp/service/repositories/languages_repository.dart';
 import 'package:trilhaapp/shared/widgets/text_label.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -11,7 +14,25 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   var nameController = TextEditingController(text: "");
   var birthDateController = TextEditingController(text: "");
+  var levelRepository = LevelRepository();
+  var levels = [];
+  var selectedLevel = "";
+  var languageRepository = LanguageRepository();
+  var languages = [];
+  var selectedLanguage = <String>[];
+  double chosenSalary = 1320;
+  int experienceTime = 0;
+  var experienceYearsRepository = ExperienceYearsRepository();
+  var experienceYears = [];
   DateTime? storedBirthDate;
+
+  @override
+  void initState() {
+    levels = levelRepository.returnLevels();
+    languages = languageRepository.returnLanguages();
+    experienceYears = experienceYearsRepository.returnExperienceYears();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +41,7 @@ class _RegisterPageState extends State<RegisterPage> {
         appBar: AppBar(title: const Text("Account")),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: ListView(
             children: [
               const TextLabel(text: "Nome"),
               const SizedBox(height: 20),
@@ -32,7 +52,6 @@ class _RegisterPageState extends State<RegisterPage> {
                   labelText: 'Nome',
                 ),
               ),
-              const SizedBox(height: 20),
               const TextLabel(text: "Data de nascimento"),
               const SizedBox(height: 20),
               TextField(
@@ -55,7 +74,68 @@ class _RegisterPageState extends State<RegisterPage> {
                   labelText: 'Data de nascimento',
                 ),
               ),
-              const SizedBox(height: 10),
+              const TextLabel(text: "Nível de experiência"),
+              Column(
+                children: levels
+                    .map((level) => RadioListTile(
+                          title: Text(level.toString()),
+                          value: level.toString(),
+                          selected: selectedLevel == level.toString(),
+                          groupValue: selectedLevel,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedLevel = value.toString();
+                            });
+                          },
+                        ))
+                    .toList(),
+              ),
+              const TextLabel(text: "Linguagens preferidas"),
+              Column(
+                  children: languages
+                      .map((language) => CheckboxListTile(
+                          title: Text(language.toString()),
+                          value: selectedLanguage.contains(language),
+                          onChanged: (bool? value) {
+                            if (value!) {
+                              setState(() {
+                                selectedLanguage.add(language);
+                              });
+                            } else {
+                              setState(() {
+                                selectedLanguage.remove(language);
+                              });
+                            }
+                          }))
+                      .toList()),
+              const TextLabel(text: "Tempo de experiência"),
+              DropdownButton(
+                  isExpanded: true,
+                  value: experienceYears.first,
+                  items: experienceYears
+                      .map((experienceYear) => DropdownMenuItem(
+                            value: experienceYear,
+                            child: Text(experienceYear.toString()),
+                          ))
+                      .toList(),
+                  onChanged: (selectedExperienceTime) {
+                    setState(() {
+                      experienceTime = selectedExperienceTime as int;
+                    });
+                  }),
+              TextLabel(text: "Pretenção salarial: R\$${chosenSalary.round().toString()}"),
+              Slider(
+                value: chosenSalary,
+                min: 1320,
+                max: 10000,
+                divisions: 500,
+                label: "R\$ 0,00",
+                onChanged: (double value) {
+                  setState(() {
+                    chosenSalary = value;
+                  });
+                },
+              ),
               TextButton(
                 onPressed: () => {
                   // Navigator.pop(context, nameController.text),
---
+++ apps/trilhaapp/lib/service/repositories/experience_years_repository.dart
@@ -0,0 +1,10 @@
+class ExperienceYearsRepository {
+  List<String> returnExperienceYears() {
+    return [
+      "Menos de 1 ano",
+      "Entre 1 e 3 anos",
+      "Entre 3 e 5 anos",
+      "Mais de 5 anos"
+    ];
+  }
+}
